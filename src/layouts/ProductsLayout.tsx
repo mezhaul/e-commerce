791112dad51c1309718793b7e0172d7a1a9b356b
kristianrobyn <kristyslittlethings@gmail.com>
2024-03-30 13:27:05 +0200

    fix:product layout

--- src/layouts/ProductsLayout.tsx
+++ src/layouts/ProductsLayout.tsx
@@ -29,22 +29,22 @@ const ProductsLayout: Component = () => {
         <div class="w-full md:w-11/12 m-auto px-2 md:px-0 pt-16 lg:pt-20 flex gap-5 py-10 mb:pb-10">
             {loading() 
                 ?
-                    <div class="w-full flex flex-wrap gap-3 justify-center">
+                    <div class="w-full flex flex-wrap gap-[9.5px]">
                     
                         {productsList().length > 0 
                             ?
                                 <For each={productsList()}>{
                                     (d) => 
-                                        <a href={`/product/${d.id}`} class="w-1/2 lg:w-1/6">
+                                        <a href={`/product/${d.id}`} class="w-1/2 lg:w-1/6 max-w-56">
                                             <div class="w-full hover:shadow bg-white border border-gray-300">
-                                                <div class="w-full bg-gray-200 overflow-hidden">
-                                                    <img src={d.images[0]} alt={d.title} class="w-full m-auto"/>
+                                                <div class="w-full h-72 overflow-hidden bg-gray-200">
+                                                    <img src={d.images[0]} alt={d.title} class=" m-auto"/>
                                                 </div>
                                                 <div class="px-2 pb-2 border-t border-gray-200">
-                                                    <h3 class="text-center text-md pt-2 pb-1 font-semibold">
+                                                    <h3 class="text-center text-md pt-2 pb-1 font-semibold line-clamp-1">
                                                         {d.name}
                                                     </h3>
-                                                    <p class="text-sm text-center font-medium">
+                                                    <p class="text-sm text-center font-medium line-clamp-1">
                                                         {d.summary}
                                                     </p>
                                                 </div>
