e7121f1f527e06a274c06dc88037dc6075b7e705
kristianrobyn <kristyslittlethings@gmail.com>
2024-06-01 18:31:22 +0200

    test mobile shop image

--- src/pages/public/Clothes.tsx
+++ src/pages/public/Clothes.tsx
@@ -1,5 +1,8 @@
 import { Component } from "solid-js";
 import LadiesImg from "../../assets/images/Ladies_Clothes_Category.jpg"
+import MenImg from "../../assets/images/Ladies_Clothes_Category.jpg"
+import ChildrenImg from "../../assets/images/Kids_Clothing_Category.jpg"
+import ToddlersImg from "../../assets/images/Ladies_Clothes_Category.jpg"
 
 
 const Clothes:Component = () => {
@@ -7,11 +10,11 @@ const Clothes:Component = () => {
         <div class="w-full md:w-11/12 m-auto px-2 md:px-0 pt-16 lg:pt-20 flex flex-wrap py-10 mb:pb-10">
             <div class="flex w-full gap-3">
                 <a href="/ladies" class="w-full md:w-1/2 bg-gray-300 h-96" style={{"background-image":`url(${LadiesImg})`,"background-size":"cover"}}></a>
-                <a href="/men" class="w-full md:w-1/2 bg-gray-300 h-96"></a>
+                <a href="/men" class="w-full md:w-1/2 bg-gray-300 h-96" style={{"background-image":`url(${MenImg})`,"background-size":"cover"}}></a>
             </div>
             <div class="flex w-full gap-3 mt-3">
-                <a href="/kids" class="w-full md:w-1/2 bg-gray-300 h-96"></a>
-                <a  href="/toddlers" class="w-full md:w-1/2 bg-gray-300 h-96"></a>
+                <a href="/kids" class="w-full md:w-1/2 bg-gray-300 h-96" style={{"background-image":`url(${ChildrenImg})`,"background-size":"cover"}}></a>
+                <a  href="/toddlers" class="w-full md:w-1/2 bg-gray-300 h-96" style={{"background-image":`url(${ToddlersImg})`,"background-size":"cover"}}></a>
             </div>
         </div>
     );
