ebbb0b9f95bc8c8045f20a01db9556ee5d05e7e9
kristianrobyn <kristyslittlethings@gmail.com>
2024-06-03 23:00:19 +0200

    fix banner

--- src/components/general/Hero.tsx
+++ src/components/general/Hero.tsx
@@ -2,7 +2,7 @@ import { Component } from 'solid-js';
 
 const Hero: Component = () => {
   return (
-    <div class="w-full flex pt-14 md:pt-0 h-[39rem] md:h-screen">
+    <div class="w-full flex pt-14 md:pt-0 h-[62.4vh] md:h-screen">
       
     </div>
   );
